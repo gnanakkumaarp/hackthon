f059456b7adefb87fa247b3d7912ec20b2559204
gnanakkumaarp <82721461+gnanakkumaarp@users.noreply.github.com>
2021-07-22 21:20:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ tellMeDiv.innerText = 'Tell us your Name! We will tell about you!';
 
 const nameInput = document.createElement('input');
 nameInput.setAttribute('id', 'name');
+nameInput.setAttribute('name', 'name');
 nameInput.setAttribute('type', 'text');
 nameInput.setAttribute('placeholder', 'Enter you Name here')
 nameInput.setAttribute('required', 'true');
@@ -27,24 +28,48 @@ result.setAttribute('class', 'on-result')
 
 document.body.append(welcomeDiv, tellMeDiv, myform, result);
 
+
+
 function getData() {
   let query = document.getElementById('name').value;
-
-  fetch(`https://api.nationalize.io?name=${query}`)
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.country.length == 3) {
-        document.getElementById('result').innerHTML = `
+  if (query.length === 0) {
+    alert('Please enter your name')
+  } else if (query.length === 1) {
+    alert('Very rare Name with single character')
+    fetch(`https://api.nationalize.io?name=${query}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.country.length == 3) {
+          document.getElementById('result').innerHTML = `
+      <div class="on-result">Hi ${data.name}, Greetings!</div> 
+      We pridicted that you are from one of countries below</div> 
+      <div class="on-result">1. ${data.country[0].country_id} with the probablity of ${((data.country[0].probability).toFixed(5))}</div>
+      <div class="on-result">2. ${data.country[1].country_id} with the probablity of ${((data.country[1].probability).toFixed(5))}</div>
+      <div class="on-result">3. ${data.country[2].country_id} with the probablity of ${((data.country[2].probability).toFixed(5))}</div>`
+        } else {
+          document.getElementById('result').innerHTML = `
+    <div class="no-result">Hi ${data.name}, Greetings!</div> 
+    <div class="no-result">We are unable to pridict about your country</div> `
+        }
+      })
+  }
+  else {
+    fetch(`https://api.nationalize.io?name=${query}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.country.length == 3) {
+          document.getElementById('result').innerHTML = `
         <div class="on-result">Hi ${data.name}, Greetings!</div> 
-        We pricdicted that you are from one of countries below</div> 
+        We pridicted that you are from one of countries below</div> 
         <div class="on-result">1. ${data.country[0].country_id} with the probablity of ${((data.country[0].probability).toFixed(5))}</div>
         <div class="on-result">2. ${data.country[1].country_id} with the probablity of ${((data.country[1].probability).toFixed(5))}</div>
         <div class="on-result">3. ${data.country[2].country_id} with the probablity of ${((data.country[2].probability).toFixed(5))}</div>`
-      } else {
-        document.getElementById('result').innerHTML = `
+        } else {
+          document.getElementById('result').innerHTML = `
       <div class="no-result">Hi ${data.name}, Greetings!</div> 
       <div class="no-result">We are unable to pridict about your country</div> `
-      }
-    })
+        }
+      })
 
+  }
 }

95732774e5124c55b1467714784c1fb33f170eec
gnanakkumaarp <82721461+gnanakkumaarp@users.noreply.github.com>
2021-07-22 19:22:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ nameInput.setAttribute('required', 'true');
 
 const readyButton = document.createElement('button');
 readyButton.setAttribute('onclick', 'getData()');
+readyButton.setAttribute('type', 'button');
+readyButton.setAttribute('return', 'false');
 readyButton.innerText = 'Tell about me';
 
 
@@ -18,15 +20,31 @@ const myform = document.createElement('form')
 myform.setAttribute('id', 'form')
 myform.append(nameInput, readyButton)
 
-document.body.append(welcomeDiv, tellMeDiv, myform);
-// let dummy = document.getElementById('name').value;
-function getData() {
-  fetch(`https://api.nationalize.io?name=kumar`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-}
 
 const result = document.createElement('div')
-result.setAttribute('class', 'result')
-const resultHeading = document.createElement('h4')
-resultHeading.innerText = `Hi, ${data.name}, your are probably from`
+result.setAttribute('id', 'result')
+result.setAttribute('class', 'on-result')
+
+document.body.append(welcomeDiv, tellMeDiv, myform, result);
+
+function getData() {
+  let query = document.getElementById('name').value;
+
+  fetch(`https://api.nationalize.io?name=${query}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.country.length == 3) {
+        document.getElementById('result').innerHTML = `
+        <div class="on-result">Hi ${data.name}, Greetings!</div> 
+        We pricdicted that you are from one of countries below</div> 
+        <div class="on-result">1. ${data.country[0].country_id} with the probablity of ${((data.country[0].probability).toFixed(5))}</div>
+        <div class="on-result">2. ${data.country[1].country_id} with the probablity of ${((data.country[1].probability).toFixed(5))}</div>
+        <div class="on-result">3. ${data.country[2].country_id} with the probablity of ${((data.country[2].probability).toFixed(5))}</div>`
+      } else {
+        document.getElementById('result').innerHTML = `
+      <div class="no-result">Hi ${data.name}, Greetings!</div> 
+      <div class="no-result">We are unable to pridict about your country</div> `
+      }
+    })
+
+}
